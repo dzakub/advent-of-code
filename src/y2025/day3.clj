902572772d5759ebb070903318e240b581c1ba8e
Dzakub <dzakub85@pm.me>
2026-04-29 13:48:53 +0200

    Second task for day 3.

--- src/y2025/day3.clj
+++ src/y2025/day3.clj
@@ -1,21 +1,31 @@
 (ns y2025.day3
   (:require [y2025.common :refer [exec]]))
 
-(defn max-joltage [line]
-  (let [bateries (map #(parse-long (str %)) (seq line))
-        first-digit (reduce max (butlast bateries))
-        remaing-battery (rest (drop-while #(not= % first-digit) bateries))
-        second-digit (reduce max remaing-battery)]
-    (+ (* 10 first-digit) second-digit)))
+(defn max-joltage2 [size line]
+  (loop [bateries (map #(parse-long (str %)) (seq line))
+         digit 0
+         depth size]
+    (if (= depth 0)
+      digit
+      (let [max-digit (reduce max (drop-last (dec depth) bateries))]
+        (recur (rest (drop-while #(not= % max-digit) bateries)) (+ (* 10 digit) max-digit) (dec depth))))))
+
+(def max-joltage-task1 (partial max-joltage2 2))
+(def max-joltage-task2 (partial max-joltage2 12))
 
 (defn solution [lines]
   (->> lines
-       (map max-joltage)
+       (map max-joltage-task1)
+       (reduce +)))
+
+(defn solution2 [lines]
+  (->> lines
+       (map max-joltage-task2)
        (reduce +)))
 
 (comment
   (exec solution "src/y2025/day3.in")
-  (max-joltage "987654321111111")
-  (max-joltage "234234234234278")
-  (max-joltage "818181911112111")
-  (max-joltage "811111111111119"))
+  (exec solution2 "src/y2025/day3.in")
+  (max-joltage2 12 "987654321111111")
+  (max-joltage2 12 "234234234234278")
+  )
